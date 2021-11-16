dc5e81f61a59ffc331061e0d47e13604cbc4c931
hiroaki.eitoku <hiroaki.eitoku@bizreach.co.jp>
2021-11-16 18:47:03 +0900

    solve chapter1

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-// import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 
-type Props = {
-  name: string
-}
-
-export const App = (props: Props) => {
+export const App = () => {
   // create count state default 0
   // example const [state, setState] = useState(default value)
+  const [count, setCount] = useState(0)
+
   // create text state default ''
+  const [text, setText] = useState("")
 
   // create functions
   // countUp count + 1
   // countDown count -1
   // cleanUp 0 reset
+  const countUp = () => {
+    setCount(count + 1)
+  }
+  const countDown = () => {
+    setCount(count - 1)
+  }
+  const cleanUp = () => {
+    setCount(0)
+  }
 
   // create side effects functions
   // example useEffect(() => {/*side effect*/}, /*dependency List*/)
   // count = 3multiple is text = 3multiple
   // count != 3multiple is text = count: {count}
 
-  // return (<>
-  //   <h1>cont: 0 or 3multiple</h1>
-  //   <button>count up</button>
-  //   <button>count down</button>
-  //   <button>count clean</button>
-  // </>)
+  useEffect(() => {
+    // そもそもの問の意図がわからなかったので答えをみた。
+    setText(count % 3 === 0 && count !== 0 ? '3multiple': `click count ${count}`)
+  }, [count])
 
-  return <h1>Hello, {props.name}</h1>
+  return (<>
+    <h1>{text}</h1>
+    <h2>count: {count}</h2>
+    <button onClick={countUp}>up</button>
+    <button onClick={countDown}>down</button>
+    <button onClick={cleanUp}>clean</button>
+  </>)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {App} from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="Hiroaki Eitoku"/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 )
