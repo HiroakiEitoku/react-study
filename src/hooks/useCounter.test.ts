59fd2d91b569dd4d8fb95f9057202d03fa85b9f7
hiroaki.eitoku <hiroaki.eitoku@bizreach.co.jp>
2021-11-22 11:37:50 +0900

    test書いてみた

--- src/hooks/useCounter.test.ts
+++ src/hooks/useCounter.test.ts
@@ -1,4 +1,5 @@
 import {RenderResult, act, renderHook} from '@testing-library/react-hooks'
+import { executionAsyncId } from 'async_hooks'
 import {useCounter, useCounterType} from './useCounter'
 
 
@@ -10,23 +11,32 @@ describe('useCounter', () => {
 
   test('default count 0', () => {
     // count get variable = result.current.count
+    expect(result.current.count).toEqual(0)
   })
   test('count up', () => {
+    expect(result.current.count).toEqual(0)
     act(() => {
       result.current.countUp()
     })
+    expect(result.current.count).toEqual(1)
   })
   test('count down', () => {
+    expect(result.current.count).toEqual(0)
     act(() => {
       result.current.countDown()
     })
+    expect(result.current.count).toEqual(-1)
   })
   test('count clean up', () => {
+    expect(result.current.count).toEqual(0)
     act(() => {
       result.current.countUp()
+      result.current.countUp()
     })
+    expect(result.current.count).toEqual(2)
     act(() => {
       result.current.cleanUp()
     })
+    expect(result.current.count).toEqual(0)
   })
 })
